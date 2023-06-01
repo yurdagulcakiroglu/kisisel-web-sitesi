fed3d361128bd8e12edb60bbb0ada49664a0d1bd
yurdagulcakiroglu <ygulcakiroglu@gmail.com>
2023-06-01 23:36:07 +0300

    new file:   img/about.jpeg 	new file:   img/blob.svg 	new file:   img/pp.png 	modified:   index.html 	modified:   style.css 	modified:   web.js

--- web.js
+++ web.js
@@ -1,9 +1,27 @@
+/* menu show y hidden*/
+const navMenu= document.getElementById('nav-menu'),
+      navToggle= document.getElementById('nav-toggle'),
+      navClose= document.getElementById('nav-close')
 
-{
-var search = document.getElementById("searchBox").value;
- window.location.href = "https://siteniz.com/" + search;
- }
+/* menu show */  
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}    
+/* menu hidden */
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
- function search() {
-    var searchQuery = document.getElementById("search-input").value;
- }
+/* Remove Menu Mobile */
+const navLink= document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu= document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n=>n.addEventListener('click',linkAction))
