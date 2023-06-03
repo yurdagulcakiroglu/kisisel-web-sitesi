13a168d7b1c357410760522c94f3797a62bb2ad1
yurdagulcakiroglu <ygulcakiroglu@gmail.com>
2023-06-03 19:25:09 +0300

    modified:   css/style.css 	modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -25,3 +25,22 @@ function linkAction(){
 }
 
 navLink.forEach(n=>n.addEventListener('click',linkAction))
+
+/* Skills */
+const skillsContent= document.getElementsByClassName('skills_content'),
+      skillsHeader= document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemClass =this.parentNode.className
+
+    for(i=0; i<skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass=== 'skills_content skills_close' ){
+        this.parentNode.className= 'skills_content skills_open'
+    }
+}    
+
+skillsHeader.forEach((e1)=>{
+    e1.addEventListener('click', toggleSkills)
+})
