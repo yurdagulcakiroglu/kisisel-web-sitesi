e5e866a72f69e3163cda8700d45741580f0068b6
yurdagulcakiroglu <ygulcakiroglu@gmail.com>
2023-06-05 00:37:25 +0300

    modified:   css/style.css     modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -82,3 +82,6 @@ function scrollUp(){
         if(this.scrollY>= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll',scrollUp)
+
+
+/*search box */
