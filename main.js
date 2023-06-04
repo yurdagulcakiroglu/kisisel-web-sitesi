c0c5722f88bd214d5bb5f404bf21b4d460cae804
yurdagulcakiroglu <ygulcakiroglu@gmail.com>
2023-06-04 17:40:45 +0300

    modified:   css/style.css 	modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -44,3 +44,41 @@ function toggleSkills(){
 skillsHeader.forEach((e1)=>{
     e1.addEventListener('click', toggleSkills)
 })
+
+/*SCROLL */
+const sections =document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY= window.pageYOffset
+
+    sections.forEach(current=>{
+        const sectionHeight= current.offsetHeight,
+              sectionTop= current.offsetTop - 50,
+              sectionId= current.getAttribute('id')
+        
+        if(scrollY> sectionTop&& scrollY<= sectionTop+ sectionHeight){
+            document.querySelector('.nav_menu a[href*='+ sectionId +']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*='+ sectionId +']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/* header arka planı */
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+
+    if(this.scrollY>= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll',scrollHeader)
+
+/*show scroll up */
+function scrollUp(){
+        const scrollUp = document.getElementById('scroll-up')
+
+        if(this.scrollY>= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
